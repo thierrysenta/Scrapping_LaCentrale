88f2c45d878faa127f7d52317c6fb1b3785c1a18
thierry <thierry.senta@hotmail.fr>
2021-12-01 11:19:06 +0100

    01/12 update

--- Scrapping_Autoscout24es_dealers.py
+++ Scrapping_Autoscout24es_dealers.py
@@ -13,7 +13,8 @@ import sys
 
 import os
 #sys.path.append(os.path.abspath("//FRSHARES0479.france.intra.corp/Corporate_Remarketing/Remarketing_Data_Intelligence/200_MARKET_INTELLIGENCE/210_WEBSCRAPPING/213_SCRAPPING/"))
-sys.path.append(os.path.abspath("C:\\Users\\PC portable\\PycharmProjects\\Scrapping_LaCentrale"))
+#sys.path.append(os.path.abspath("C:\\Users\\PC portable\\PycharmProjects\\Scrapping_LaCentrale"))
+sys.path.append(os.path.abspath("C:\\Users\\c02662\\PycharmProjects\\Scrapping_LaCentrale"))
 import Scrapping_def
 
 
@@ -30,8 +31,8 @@ import json
 from pandas.io.json import json_normalize
 import pandas as pd
 
-#proxies  = {"http":"http://{user}:{pwd}@primaryprx.intra.corp:8090".format(user=user,pwd=password),"https":"http://{user}:{pwd}@primaryprx.intra.corp:8090".format(user=user,pwd=password)}
-#print(proxies)
+proxies  = {"http":"http://{user}:{pwd}@primaryprx.intra.corp:8090".format(user=user,pwd=password),"https":"http://{user}:{pwd}@primaryprx.intra.corp:8090".format(user=user,pwd=password)}
+print(proxies)
 
 url = "https://profesionales.autoscout24.es/DealersSearch/Find"
 
@@ -42,15 +43,15 @@ for elm in range(1,32):
         headers = {
           'Content-Type': 'application/json'
         }
-        #response = requests.post(url, headers=headers, json=payload,proxies = proxies, verify  = False)
-        response = requests.post(url, headers=headers, json=payload,verify  = False)
+        response = requests.post(url, headers=headers, json=payload,proxies = proxies, verify  = False)
+        #response = requests.post(url, headers=headers, json=payload,verify  = False)
         # print(response.text)
         dealers.extend(response.json()['dealers'])
     except:
         pass
 dealers = json_normalize(dealers)
 
-dealer_list = dealers["UrlName"][878:].tolist()
+dealer_list = dealers["UrlName"].tolist()
 
 Json_dataframe = pd.DataFrame()
 j=0
@@ -61,14 +62,25 @@ for i in dealer_list:
     print(str(j) + " - " + str(i))
     url = "https://www.autoscout24.es/profesionales/{}?srcl=2&intpgat=dealersearch-home&ipc=dealersearch%7Cdealerinfo&ipl=result".format(i)
     print(url)
-    soup_mysite = Scrapping_def.DOM_extract(user , password, url, chrome = False, sleep = False)
-    test = soup_mysite.find("script",{"id":"initial-state"}).contents[0]
-    Json_dftp = json_normalize(json.loads(test)["dealerInfoPage"])
-    Json_dataframe = Json_dataframe.append(Json_dftp)
+    try:
+        soup_mysite = Scrapping_def.DOM_extract(user , password, url, chrome = False, sleep = False)
+        test = soup_mysite.find("script",{"id":"initial-state"}).contents[0]
+        Json_dftp = json_normalize(json.loads(test)["dealerInfoPage"])
+        Json_dataframe = Json_dataframe.append(Json_dftp)
+    except:
+        print("erreur sur "+str(i))
+        pass
 
-Json_dataframe_877 = Json_dataframe
+# Merging dealers (post) and scrapping js
 
-Json_dataframe_export = Json_dataframe_877.append(Json_dataframe)
-Json_dataframe_export.to_csv("C:\\Users\\PC portable\\PycharmProjects\\Scrapping_LaCentrale\\Annuaire_Autooscout24es_dealers.csv", index=False, sep=";", encoding="utf-8")
-    
-dealers.to_excel('//FRSHARES0479.france.intra.corp/Corporate_Remarketing/Remarketing_Data_Intelligence/200_MARKET_INTELLIGENCE/210_WEBSCRAPPING/213_SCRAPPING/dealers_es.xlsx')
+dealers.sort_values('UrlName',inplace=True)
+dealers_unique = dealers.drop_duplicates(subset =["UrlName",'RatingsInfoUrl','GeoLocation.Longitude', 'GeoLocation.Latitude'])
+Json_dataframe.sort_values('slug',inplace=True)
+Json_dataframe_unique = Json_dataframe.drop_duplicates(subset =['slug','customerName','homepageUrl','companyAddOn','aboutUs','imprint', 'customerAddress.country','customerAddress.zipCode', 'customerAddress.city'])
+
+
+
+dealers_final = Json_dataframe_unique[['slug','customerName','customerPhoneNumbers','callPhoneNumbers','homepageUrl','companyAddOn','aboutUs','imprint', 'customerAddress.country','customerAddress.zipCode', 'customerAddress.city', 'customerAddress.street', 'ratings.ratingAverage', 'ratings.ratingStars', 'logo.url']].merge(dealers_unique[["UrlName",'RatingsInfoUrl','GeoLocation.Longitude', 'GeoLocation.Latitude']], how='left', left_on=['slug'], right_on=['UrlName']) # how='left'
+dealers_final.sort_values('slug',inplace=True)
+dealers_final.to_excel('\\\\FRSHARES0479.france.intra.corp\\Corporate_Remarketing\\Remarketing_Data_Intelligence\\300_EFFICIENCY_TOOLS\\380_PROSPECTION\\00_DATA\\dealers_autoscout24es.xlsx') 
+
--- Scrapping_def.py
+++ Scrapping_def.py
@@ -64,9 +64,9 @@ def get_chrome_driver(user, password):
     # Création du navigateur headless
     chrome_driver_path = "C:\\Users\\PC portable\\PycharmProjects\\Scrapping_LaCentrale\\chromedriver.exe"
     service_args = []
-    #service_args = [
-    #    '--proxy=http://{}:{}@frmarprx0101.france.intra.corp:8090'.format(user, password),
-    #    '--proxy-type=https']
+    service_args = [
+        '--proxy=http://{}:{}@primaryprx.france.intra.corp:8090'.format(user, password),
+        '--proxy-type=https']
     options = Options()
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
@@ -106,8 +106,9 @@ def DOM_extract(user, password, url, chrome=False, sleep=False, debug=False):
 
     else:
         # Recupération du code HTML de la page "description détaillé" de la voiture
-        #htmlSource = proxy_test(user, password, url)
+        htmlSource = proxy_test(user, password, url)
         # sans proxy :
-        htmlSource = urllib.request.urlopen(url).read()
+        #htmlSource = urllib.request.urlopen(url).read()
     # Lecture du code HTML par le module Beautifulsoup
-    return BeautifulSoup(htmlSource, "lxml")
+    return BeautifulSoup(htmlSource, "html.parser")    
+    #return BeautifulSoup(htmlSource, "lxml")
